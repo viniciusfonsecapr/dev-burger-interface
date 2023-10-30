9d94fc8711544a833e71d25e3f8001bc55bc1041
Vinicius Fonseca <vinicius.fonseca20093004@gmail.com>
2023-10-30 00:24:08 -0300

    primeiro commit deploy

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from  'axios'
 
 
 const apiDevBuger = axios.create({
-    baseURL: 'http://localhost:3001/'
+    baseURL: 'https://dev-club-burger-production.up.railway.app/'
 })
 
 apiDevBuger.interceptors.request.use( async config => {
