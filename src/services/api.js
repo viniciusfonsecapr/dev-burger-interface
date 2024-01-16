84cb1fe50c4e8c38f159f674841151ec66672dc8
Vinicius Fonseca <vinicius.fonseca20093004@gmail.com>
2024-01-15 23:50:10 -0300

    Ajustando api

--- src/services/api.js
+++ src/services/api.js
@@ -4,9 +4,6 @@ import axios from  'axios'
 const apiDevBuger = axios.create({
      baseURL: 'https://dev-club-burger-production.up.railway.app/',
      
-
-   
-    
 })
 
 apiDevBuger.interceptors.request.use( async config => {
